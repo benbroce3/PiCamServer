2dd4649c0539ad6d46c40ee467e0310cf05c52c7
technoscientist <themetallicbros@gmail.com>
2016-05-19 12:42:11 -0400

    Add files via upload

---
+++ passHandler.php
@@ -0,0 +1,18 @@
+<!doctype html>
+	<html>
+	<body>
+	<h2>hi<h2>
+		<?php
+			//password variable
+			$password = "bo";
+			echo "yoyo";
+			echo $_POST['pass'];
+			if ($_POST['pass'] = $password)
+			{
+				header(location: 'https://www.google.com');
+
+			}
+		?>
+
+	</body>
+	</html>
