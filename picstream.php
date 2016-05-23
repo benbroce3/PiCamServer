839c27b2b00af0f6d6f00ba1bbea0324b8db0cb8
technoscientist <themetallicbros@gmail.com>
2016-05-23 13:15:11 -0400

    Update picstream.php

--- picstream.php
+++ picstream.php
@@ -14,8 +14,8 @@
 		?>
 		<script>
 			
-			var mytimer = setInterval(reload, 10000);
-			 function reload()
+			setInterval(myTimer, 10000);
+			 function myTimer()
 			 {
 			 	location.reload(); 
 			 }
