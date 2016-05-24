09e00e3e07b882cddaa63645279484410109e601
Benjamin Broce <benbroce2@gmail.com>
2016-05-23 22:09:29 -0400

    Match format of vidstream.php to picstream.php

--- vidstream.php
+++ vidstream.php
@@ -1,9 +1,24 @@
 <DOCTYPE! html>
 <html>
-	<head>
+	<header>
 		<title>Video Stream</title>
-	</head>
+		<!--Bootstrap Setup-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+		<!--<script src="bootstrap/bootstrap.min.js"></script>-->
+	</header>
 	<body>
-		<center><h1>Video Stream:</h1></center>
+		<div class="container-fluid">
+			<!--Navigation Tabs-->
+			<div class="row">
+				<div class="col-md-12">
+					<ul class="nav nav-tabs" style="text-align:center">
+						<li role="presentation" class="active"><a href="vidstream.php">Video Stream</a></li>
+						<li role="presentation"><a href="picstream.php">Picture Stream</a></li>
+						<li role="presentation"><a href="cgi-bin/config.py">Configuration</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
