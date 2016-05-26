040a1d238fc65725e5f2aea4699d90559a96cc07
Benjamin Broce <benbroce2@gmail.com>
2016-05-26 12:55:07 -0400

    put in cgi-bin and fix referencing

--- cgi-bin/vidstream.py
+++ cgi-bin/vidstream.py
@@ -18,21 +18,21 @@ print '''
 			<title>Video Stream</title>
 			<!--Bootstrap Setup-->
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" href="bootstrap/bootstrap.min.css">
-			<!--<script src="bootstrap/bootstrap.min.js"></script>-->
+			<link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+			<!--<script src="../bootstrap/bootstrap.min.js"></script>-->
 		</head>
 		<body>
 			<div class="container-fluid">
 				<!--Navigation Tabs-->
 				<div class="row">
-					<div class="col-md-12">
-						<center>
-							<ul class="nav nav-tabs" style="text-align:center">
-								<li role="presentation" class="active"><a href="vidstream.py">Video Stream</a></li>
-								<li role="presentation"><a href="picstream.php">Picture Stream</a></li>
-								<li role="presentation"><a href="cgi-bin/config.py">Configuration</a></li>
-							</ul>
-						</center>
+					<div class="col-md-offset-4 col-md-4">
+						<ul class="nav nav-tabs" style="text-align:center">
+							<li role="presentation" class="active"><a href="vidstream.py">Video Stream</a></li>
+							<li role="presentation"><a href="../picstream.php">Picture Stream</a></li>
+							<li role="presentation"><a href="config.py">Configuration</a></li>
+						</ul>
+					</div>
+					<div class="col-md-4">
 					</div>
 				</div>
 				<!--Video-->
