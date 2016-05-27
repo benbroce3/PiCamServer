d2c612fb73d3e581312a758ab4dcee9a70914410
Benjamin Broce <benbroce2@gmail.com>
2016-05-27 12:24:34 -0400

    Update temp_vidstream.py

--- temp_vidstream.py
+++ temp_vidstream.py
@@ -1,8 +1,9 @@
 import picamera
 from picamera import PiCamera
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.start_recording('vids/vidstream.h264')
-    camera.wait_recording(60)
-    camera.stop_recording()
+camera = PiCamera()
+    
+camera.resolution = (640, 480)
+camera.start_recording('/var/www/PiCamServer/vids/vidstream.h264')
+camera.wait_recording(60)
+camera.stop_recording()
