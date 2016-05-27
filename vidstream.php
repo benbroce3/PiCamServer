716425057dae8b431a8eb8d926236ff1e8a093fe
technoscientist <themetallicbros@gmail.com>
2016-05-27 12:14:18 -0400

    Update vidstream.php

--- vidstream.php
+++ vidstream.php
@@ -35,7 +35,7 @@
 			</div>
 			<!--Video-->
 	      		<video width="640" height="480" autoplay>
-	       		<source src="vidstream.mp4" type="video/mp4">\
+	       		<source src="vidstream.mp4 type="video/mp4">
 	     		</video>
 		</div>
 	</body>
