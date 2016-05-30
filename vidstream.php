f4532084e901188dc0107558840ec409a02a0d2a
Benjamin Broce <benbroce2@gmail.com>
2016-05-30 12:18:57 -0400

    Update vidstream.php

--- vidstream.php
+++ vidstream.php
@@ -41,7 +41,7 @@
 	      			var vlc = document.getElementById("vlc");
 	      			//https://wiki.videolan.org/Documentation:WebPlugin/
 	      			//http://stackoverflow.com/questions/14375767/embedding-vlc-plugin-on-html-page
-	      			//vlc.input.time = vid.vlc.input.length - 3;
+	      			vlc.input.time = vid.vlc.input.length - 3;
 	      			//vlc.input.position = 0.9;
 			</script>
 		</div>
