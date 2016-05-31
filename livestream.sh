f1aa52d5b76dd6db5259cd23936294da487295c3
Benjamin Broce <benbroce2@gmail.com>
2016-05-31 15:23:39 -0400

    Update livestream.sh

--- livestream.sh
+++ livestream.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-raspivid -o - -t 0 -w 600 -h 400 -fps 12 -vf | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
+raspivid -o - -t 0 -w 640 -h 480 -fps 30 -vf | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
