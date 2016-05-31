252d873016ded932d5060eeed5839f95c285bead
Benjamin Broce <benbroce2@gmail.com>
2016-05-30 23:16:48 -0400

    Fix Up

--- camerav3.py
+++ camerav3.py
@@ -12,9 +12,7 @@ camera.resolution = (640, 480)
 
 print "\nSecurity Camera Logger v3 | Ben Broce & William Hampton\n\n"
 print "Streams video to vids/vidstream.h264 | Captures to pics/[timestamp].jpg"
-print "Ctrl-C quits.\n\n"  if stream == "y":
-    camera.stop_recording()
-  camera.stop_preview()
+print "Ctrl-C quits.\n\n"
 
 stream = raw_input("Should I stream video (y/n)? ")
 length = float(raw_input("How long should I run (in minutes): "))*60
@@ -25,18 +23,18 @@ print "Running..."
 camera.annotate_background = picamera.Color('black')
 
 try:
-  camera.start_preview()
-  if stream == "y":
-    camera.start_recording('/var/www/vids/vidstream.h264')
-  while (counter <= length):
-    timestamp = datetime.now().strftime("%m-%d-%Y_%H:%M:%S")
-    camera.annotate_text = timestamp
-    path = '/var/www/PiCamServer/pics/' + timestamp + '.jpg'
-    camera.capture(path, use_video_port=True)
-    time.sleep(interval)
-    counter += interval
+	camera.start_preview()
+	if stream == "y":
+		camera.start_recording('/var/www/vids/vidstream.h264')
+	while (counter <= length):
+		timestamp = datetime.now().strftime("%m-%d-%Y_%H:%M:%S")
+		camera.annotate_text = timestamp
+		path = '/var/www/PiCamServer/pics/' + timestamp + '.jpg'
+		camera.capture(path, use_video_port=True)
+		time.sleep(interval)
+		counter += interval
 finally:
-  print "Exiting..."
-  if stream == "y":
-    camera.stop_recording()
-  camera.stop_preview()
+	print "Exiting..."
+	if stream == "y":
+		camera.stop_recording()
+	camera.stop_preview()
