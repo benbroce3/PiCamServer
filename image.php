7029622d1d7311de0bf2b6b8f8af59eeddd0005b
Benjamin Broce <benbroce2@gmail.com>
2016-06-02 13:30:15 -0400

    Update image.php

--- image.php
+++ image.php
@@ -1,11 +1,7 @@
 <?php
         //set the content type header as jpeg
         header('Content-Type: image/jpeg');
-        
-        $images = array_reverse(glob("../pics/*.jpg"));
-        
-        //recursively display all images in "pics" dir
-        foreach($images as $image) {
+        $image = "../pics/05-31-2016_15:32:04.jpg";
         	//output image
         	imagejpeg($image);
         	//free up memory
