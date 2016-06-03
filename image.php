2e28360e542f78ea0099f1d835980b9394e9ed3e
Benjamin Broce <benbroce2@gmail.com>
2016-06-03 12:03:06 -0400

    Update image.php

--- image.php
+++ image.php
@@ -1,11 +1,8 @@
-<?php
-        error_reporting(0);
-        //set the content type header as jpeg
-        header('Content-Type: image/jpeg');
-        $image = "../pics/05-31-2016_15:32:04.jpg";
-        	//output image
-        	imagejpeg($image);
-        	//free up memory
-        	imagedestroy($image);
-        }
-?>
+error_reporting(0);
+//set the content type header as jpeg
+header('Content-Type: image/jpeg');
+$image = "../pics/05-31-2016_15:32:04.jpg";
+//output image
+imagejpeg($image);
+//free up memory
+imagedestroy($image);
