73472999762f77ec312c16465b8d89fb032ae21e
Benjamin Broce <benbroce2@gmail.com>
2016-06-03 12:07:13 -0400

    Update image.php

--- image.php
+++ image.php
@@ -2,8 +2,6 @@
 <html>
   <body>
     <?php
-      error_reporting(0);
-      //set the content type header as jpeg
       header('Content-Type: image/jpeg');
       $image = "../pics/05-31-2016_15:32:04.jpg";
       //output image
