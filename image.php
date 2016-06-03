11a77e16756789a478a30ac8a8346faec9ef9e07
Benjamin Broce <benbroce2@gmail.com>
2016-06-03 12:08:25 -0400

    Update image.php

--- image.php
+++ image.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <?php
-      header('Content-Type: image/jpeg');
-      $image = "../pics/05-31-2016_15:32:04.jpg";
-      //output image
-      imagejpeg($image);
-      //free up memory
-      imagedestroy($image);
-    ?>
-  </body>
-</html>
+<?php
+  header('Content-Type: image/jpeg');
+  $image = "../pics/05-31-2016_15:32:04.jpg";
+  //output image
+  imagejpeg($image);
+  //free up memory
+  imagedestroy($image);
+?>
