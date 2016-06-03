9c99dea8ca8015aa3424a4b8fd252efd6e9b11c6
Benjamin Broce <benbroce2@gmail.com>
2016-06-03 12:38:50 -0400

    Update image.php

--- image.php
+++ image.php
@@ -1,9 +1,16 @@
 <?php
   header('Content-Type: image/jpeg');
-  $src = "../pics/05-31-2016_15:32:04.jpg";
-  $image = imagecreatefromjpeg($src);
-  //output image
-  imagejpeg($image,NULL);
-  //free up memory
-  imagedestroy($image);
+  $image = "../pics/05-31-2016_15:32:04.jpg";
+  $src = imagecreatefromjpeg($image);
+  $new_width=480;
+  $new_height=360;
+  $w_src=1920;
+  $h_src=1080;
+  $img=imagecreatetruecolor($new_width,$new_height);
+  imagecopyresampled($img,$src,0,0,0,0,$new_width,$new_height,$w_src,$h_src);
+  imagejpeg($img,NULL,45);
+  //$cacheFile=$cache_dir.$cacheFile;
+  //imagejpeg($img,$cacheFile,45);
+  imagedestroy($src);
+  imagedestroy($img);
 ?>
