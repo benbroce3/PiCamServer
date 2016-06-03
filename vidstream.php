4998ef3c5c6dcac6ce5ef74004360042f3d36f55
Benjamin Broce <benbroce2@gmail.com>
2016-06-03 13:40:26 -0400

    finalize for v1

--- vidstream.php
+++ vidstream.php
@@ -24,11 +24,12 @@
 			<!--Navigation Tabs-->
 			<div class="row">
 				<div class="col-md-offset-4 col-md-4">
-					<ul class="nav nav-tabs" style="text-align:center">
-						<li role="presentation" class="active"><a href="vidstream.php">Video Stream</a></li>
-						<li role="presentation"><a href="picstream.php">Picture Stream</a></li>
-						<li role="presentation"><a href="cgi-bin/config.cgi">Configuration</a></li>
-					</ul>
+					<center>
+						<ul class="nav nav-tabs" style="text-align:center">
+							<li role="presentation" class="active"><a href="vidstream.php">Video Stream</a></li>
+							<li role="presentation"><a href="picstream.php">Picture Stream</a></li>
+						</ul>
+					</center>
 				</div>
 				<div class="col-md-4">
 				</div>
