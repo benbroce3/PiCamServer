2ec1d5ec4ebfe6f6b809181e75b649aff46ac31c
Benjamin Broce <benbroce2@gmail.com>
2016-06-06 08:52:45 -0400

    fix nav spacing

--- picstream.php
+++ picstream.php
@@ -23,13 +23,13 @@
 		<div class="container-fluid">
 			<!--Navigation Tabs-->
 			<div class="row">
-				<div class="col-md-offset-5 col-md-4">
+				<div class="col-md-offset-5 col-md-2">
 					<ul class="nav nav-tabs" style="text-align:center">
 						<li role="presentation"><a href="vidstream.php">Video Stream</a></li>
 						<li role="presentation" class="active"><a href="picstream.php">Picture Stream</a></li>
 					</ul>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-5">
 				</div>
 			</div>
 			<!--Pictures-->
