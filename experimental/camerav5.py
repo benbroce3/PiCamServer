b7ee3973c4480515177ffb5e757e85ec28f73631
Benjamin Broce <benbroce2@gmail.com>
2016-06-07 12:29:03 -0400

    start changing to python3

--- experimental/camerav5.py
+++ experimental/camerav5.py
@@ -5,16 +5,17 @@ from picamera import PiCamera
 import time
 from datetime import datetime
 import os.path
-from subprocess32 import Popen
+from subprocess import Popen
 
-print "\nSecurity Camera Logger v4 | Ben Broce & William Hampton\n"
-print "Streams video to rtsp://pi-ip:8554/ | Captures to pics/[timestamp].jpg"
-print "Ctrl-C quits.\n"
+print("\nPiCamServer Camera Backend v5\n")
+print("Streams video to rtsp://pi-ip:8554/ | Captures to pics/[timestamp].jpg")
+print("Ctrl-C quits.\n")
 
-stream = raw_input("Should I stream video or take pictures (v/p)? ")
-preview = raw_input("Should I display video preview on Pi (y/n)? ")
+config_now = input("Configure session settings now (y/n)? ")
+stream = input("Should I stream video or take pictures (v/p)? ")
+preview = input("Should I display video preview on Pi (y/n)? ")
 
-print "Running..."
+print("Running...")
 
 #http://www.raspberry-projects.com/pi/pi-hardware/raspberry-pi-camera/streaming-video-using-vlc-player
 #http://www.diveintopython.net/scripts_and_streams/stdin_stdout_stderr.html
@@ -24,11 +25,11 @@ if (stream == "v" or stream == "V"):
 	try:
 		live = Popen(["./livestream.sh"])
 	finally:
-		print "\n\nExiting..."
+		print("\n\nExiting...")
 		live.terminate()
 elif (stream == "p" or stream == "P"):
-	length = float(raw_input("How long should I run (in minutes): "))*60
-	interval = float(raw_input("How often should I take a picture (in seconds): "))
+	length = float(input("How long should I run (in minutes): "))*60
+	interval = float(input("How often should I take a picture (in seconds): "))
 	
 	camera = PiCamera()
 	camera.annotate_background = picamera.Color('black')
@@ -48,8 +49,8 @@ elif (stream == "p" or stream == "P"):
 			time.sleep(interval)
 			counter += interval
 	finally:
-		print "Exiting..."
+		print("Exiting...")
 		if (preview == "y" or preview == "Y"):
 			camera.stop_preview()
 else:
-	print "Invalid input!"
+	print("Invalid input!")
