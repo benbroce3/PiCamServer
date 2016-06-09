9689cd28d08882cc64f93d01d21929adc917b65f
Benjamin Broce <benbroce2@gmail.com>
2016-06-09 09:45:36 -0400

    Update vidstream.php

--- vidstream.php
+++ vidstream.php
@@ -32,7 +32,6 @@
 			</div>
 			<!--Video-->
 			<center>
-				<br>
 				<embed type="application/x-vlc-plugin" text="Waiting for stream..." pluginspage="http://www.videolan.org" target="rtsp://192.168.0.236:8554/" height="480" width="640"/>
 			</center>
 		</div>
