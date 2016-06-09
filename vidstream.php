d2f2633158295dfdcd0f19d5ede47528b3891533
Benjamin Broce <benbroce2@gmail.com>
2016-06-09 09:44:33 -0400

    fix spacing

--- vidstream.php
+++ vidstream.php
@@ -32,6 +32,7 @@
 			</div>
 			<!--Video-->
 			<center>
+				<br>
 				<embed type="application/x-vlc-plugin" text="Waiting for stream..." pluginspage="http://www.videolan.org" target="rtsp://192.168.0.236:8554/" height="480" width="640"/>
 			</center>
 		</div>
