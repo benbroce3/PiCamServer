2df1a506c6ace3364aade4650b9596449fd2f0e5
Benjamin Broce <benbroce2@gmail.com>
2016-06-09 08:35:23 -0400

    rm image.php (not working, switching to html pics)

--- experimental/image.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header('Content-Type: image/jpeg');
-	$src = "05-31-2016_15:31:51.jpg";
-	//$image = "../pics/05-31-2016_15:32:04.jpg";
-	$image = imagecreatefromjpeg($src);
-	imagedestroy($image);
-?>
